4ecafcdc4896c60cf8fda92d7fca9ef2be679957
FractalXX <fade@hotmail.hu>
2018-01-07 20:47:25 +0100

    Add socketer tile and item

---
+++ Items/Socketer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VapeRPG.Items
+{
+    public class Socketer : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 36;
+            item.height = 30;
+            item.maxStack = 99;
+
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.rare = 5;
+            item.value = 150;
+            item.createTile = mod.TileType<Tiles.Socketer>();
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Socketer");
+            Tooltip.SetDefault("Used for inserting gems in weapons.");
+        }
+
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 1);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/Socketer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Terraria.ModLoader;
+
+using VapeRPG.Items;
+
+namespace VapeRPG.Tiles
+{
+    public class Socketer : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolidTop[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = false;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Width = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 18 };
+            TileObjectData.addTile(Type);
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+            adjTiles = new int[] { TileID.WorkBenches };
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 36, 30, mod.ItemType<Items.Socketer>());
+        }
+    }
+}
