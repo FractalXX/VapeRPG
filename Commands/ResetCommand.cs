78d5ab2ed0a82ac2ab61e0cae7309b89159bda5e
Stefano-De-Cesare <65348662+Stefano-De-Cesare@users.noreply.github.com>
2021-11-22 00:40:46 -0300

    Added command to reset bugged out characters

---
+++ Commands/ResetCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using VapeRPG;
+
+using Microsoft.Xna.Framework;
+
+namespace VapeRPG.Commands
+{
+    public class ResetCommand : ModCommand
+	{
+		public override CommandType Type
+			=> CommandType.Chat;
+
+		public override string Command
+			=> "resetCharacter";
+
+        public override string Usage
+            => "/resetCharacter";
+
+		public override string Description
+			=> "Resets your Vape RPG Character to level 1.";
+
+		public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            VapePlayer player = caller.Player.GetModPlayer<VapePlayer>();
+			if(player != null)
+            {
+                player.level = 1;
+                player.chaosRank = 1;
+
+                player.xp = 0;
+                player.chaosXp = 0;
+
+                player.statPoints = 0;
+                player.chaosPoints = 0;
+
+                foreach (string stat in VapeRPG.BaseStats)
+                {
+                    player.BaseStats[stat] = 1;
+                }
+
+                foreach (Skill skill in VapeRPG.Skills)
+                {
+                    player.SkillLevels[skill.name] = 0;
+                }
+            }
+        }
+    }
+}
